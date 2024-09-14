a6daaf1b0bbef2daac6ed70ba329e26aca19610a
João Florão <joaovitor.florao@gmail.com>
2024-09-14 19:27:10 -0300

    [ADD] Adição da importação da variáveis globais db e login_manager

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,8 @@
 from .user_service import UserService
 from .animal_service import AnimalService
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
