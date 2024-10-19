16f646b9c63eb561105b980d0bf133fe102cbc06
João Florão <joaovitor.florao@gmail.com>
2024-10-19 19:39:46 -0300

    [IMP] Implementação de melhoria na lógica de cadastro do histórico do animal

--- app/controllers/animal_history_controller.py
+++ app/controllers/animal_history_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, flash, session, redirect, url_for
 from flask_login import login_required, current_user
 from app import animal_history_service, animal_service
+from app.models.event_type import EventType
 
 bp = Blueprint("animal_history", __name__)
 
@@ -9,8 +10,14 @@ bp = Blueprint("animal_history", __name__)
 @login_required
 def animal_history():
     data = request.form
-    animal_id = data.get("nAnimalId")
     action_types_list = animal_history_service.get_event_types()
+    status_list = animal_service.get_status_list()
+
+    animal_id = data.get("nAnimalId", type=int)
+    if request.method == "GET":
+        animal = animal_service.get_animal_by_id(animal_id)
+        animal_history = animal_history_service.get_animal_history(animal_id)
+        return render_template("animal_history.html", **locals())
 
     animal = animal_service.get_animal_by_id(animal_id)
     animal_history = animal_history_service.get_animal_history(animal_id)
@@ -23,15 +30,26 @@ def animal_history():
 def register_location():
     data = request.form
     action_types_list = animal_history_service.get_event_types()
+    status_list = animal_service.get_status_list()
 
-    animal_id = data.get("nAnimalId")
+    animal_id = data.get("nAnimalId", type=int)
+    if request.method == "GET":
+        animal = animal_service.get_animal_by_id(animal_id)
+        animal_history = animal_history_service.get_animal_history(animal_id)
+        return render_template("animal_history.html", **locals())
+
     action_type = data.get("nActionType")
     description = data.get("nDescription")
-    start_date = data.get("nStartDate")
-    end_date = data.get("nEndDate")
+    event_date = data.get("nEventDate")
+    animal_new_status = data.get("nAnimalStatus")
 
+    event_description = description
     animal = animal_service.get_animal_by_id(animal_id)
-    animal_history_service.add_animal_event_history(animal_id, action_type, description, start_date, end_date)
+    if action_type == EventType.status.value:
+        animal_service.update_animal_status(animal_id, animal_new_status)
+        event_description = f"Animal {animal_new_status}. {description}"
+
+    animal_history_service.add_animal_event_history(animal_id, action_type, event_description, event_date)
     animal_history = animal_history_service.get_animal_history(animal_id)
 
-    return render_template("animal_history.html", **locals())
+    return redirect(url_for('animal_history', animal_id=animal_id))
--- app/models/animal_history.py
+++ app/models/animal_history.py
@@ -11,14 +11,12 @@ class AnimalHistory(db.Model):
     animal_id = db.Column(db.Integer, db.ForeignKey('animal.id'), nullable=False)
     action_type = db.Column(SqlEnum(EventType), nullable=False)
     description = db.Column(db.String, nullable=False)
-    start_date = db.Column(db.DateTime, default=datetime.utcnow)
-    end_date = db.Column(db.DateTime, nullable=True)
+    event_date = db.Column(db.Date)
 
-    def __init__(self, animal_id, action_type, description, start_date, end_date):
+    def __init__(self, animal_id, action_type, description, event_date):
         self.animal_id = animal_id
         self.action_type = EventType(action_type)
         self.description = description
-        self.start_date = start_date
-        self.end_date = end_date or None
+        self.event_date = event_date
 
-    animals_reference = db.relationship('Animal', back_populates='location_history')
+    animals_reference = db.relationship("Animal", back_populates="location_history")
